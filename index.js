84aa34d0925b1b493f9590069746b85319446393
Solomon Amsalu <solamsalu@gmail.com>
2024-10-03 23:40:39 +0300

    index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
-require('dotenv').config(); // Load .env file
+// require('dotenv').config(); // Load .env file
+BOT_TOKEN='7639349507:AAEMg5R2hjjFZv1ByT_1aDZeNR9kY_Wc460';
 
 const TeleBot = require('telebot'); // Assuming you already have this import
 const BOT_TOKEN = process.env.BOT_TOKEN; // Use the token from the .env file
