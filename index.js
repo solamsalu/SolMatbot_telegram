b882df1241d550bf5825a07b683c35bbb02dce61
Solomon Amsalu <solamsalu@gmail.com>
2024-10-03 23:01:22 +0300

    index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+console.log('All Environment Variables:', process.env); // Debug step
 console.log('Bot Token:', process.env.BOT_TOKEN);
 
 const TeleBot = require('telebot');
