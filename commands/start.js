c0997fcbc61c9f73ccdb7db4b24e6d1f2cc7059e
Solomon Amsalu <solamsalu@gmail.com>
2024-10-11 18:00:10 +0300

    Index update

--- commands/start.js
+++ commands/start.js
@@ -20,15 +20,41 @@
 //  }
 
 
-const canUseBot = require('../utils/canUseBot'); 
+// const canUseBot = require('../utils/canUseBot'); 
+
+// module.exports = (bot) => {
+//     bot.on('/start', async (msg) => {
+//         if (await canUseBot(msg.from.id, msg.chat.id)) {
+//             bot.sendMessage(msg.chat.id, `<b>Welcome to the Mathematics Teaching Bot!</b>\nUse the following commands to get started:`, {
+//                 replyMarkup: bot.inlineKeyboard([
+//                     [bot.inlineButton('📘 Calculus I (Math2021)', { callback: 'calculus1_start' })],
+//                     // [bot.inlineButton('📘 Calculus (Math1002)', { callback: 'calculus_start' })],
+//                     [bot.inlineButton('📗 Algebra (Math1021)', { callback: 'algebra_start' })],
+//                     [bot.inlineButton('💬 Discussion Forum', { callback: 'discussion_button' })],
+//                     [bot.inlineButton('📅 Upcoming Events', { callback: 'events_button' })],
+//                     [bot.inlineButton('🧮 Daily Math Problem', { callback: 'daily_problem_button' })],
+//                     [bot.inlineButton('📞 Contact Support', { callback: 'contact_button' })],
+//                     [bot.inlineButton('❓ Help', { callback: 'help_button' })]
+//                 ]),
+//                 parseMode: 'HTML' // Specify parse mode as HTML
+//             });
+//         }
+//     });
+// };
+
+
+const canUseBot = require('../utils/canUseBot');
 
 module.exports = (bot) => {
     bot.on('/start', async (msg) => {
-        if (await canUseBot(msg.from.id, msg.chat.id)) {
-            bot.sendMessage(msg.chat.id, `<b>Welcome to the Mathematics Teaching Bot!</b>\nUse the following commands to get started:`, {
+        const userId = msg.from.id; // Get user ID
+        const chatId = msg.chat.id; // Get chat ID
+        
+        const canUse = await canUseBot(userId, chatId); // Check if the user can use the bot
+        if (canUse) {
+            bot.sendMessage(chatId, `<b>Welcome to the Mathematics Teaching Bot!</b>\nUse the following commands to get started:`, {
                 replyMarkup: bot.inlineKeyboard([
                     [bot.inlineButton('📘 Calculus I (Math2021)', { callback: 'calculus1_start' })],
-                    // [bot.inlineButton('📘 Calculus (Math1002)', { callback: 'calculus_start' })],
                     [bot.inlineButton('📗 Algebra (Math1021)', { callback: 'algebra_start' })],
                     [bot.inlineButton('💬 Discussion Forum', { callback: 'discussion_button' })],
                     [bot.inlineButton('📅 Upcoming Events', { callback: 'events_button' })],
