34c524df08a0cb421ecf854c2e6c4ca16614fac2
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2025-02-17 12:10:09 +0300

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -56,6 +56,7 @@ module.exports = (bot) => {
                 replyMarkup: bot.inlineKeyboard([
                     [bot.inlineButton('📘 Calculus I (Math2021)', { callback: 'calculus1_start' })],
                     [bot.inlineButton('📗 Algebra (Math1021)', { callback: 'algebra_start' })],
+                    [bot.inlineButton('📗 Applied Math III (Math2072)', { callback: 'appliedmath3_start' })],
                     [bot.inlineButton('💬 Discussion Forum', { callback: 'discussion_button' })],
                     [bot.inlineButton('📅 Upcoming Events', { callback: 'events_button' })],
                     [bot.inlineButton('🧮 Daily Math Problem', { callback: 'daily_problem_button' })],
