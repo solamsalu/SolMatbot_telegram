67877ffb9e94e0f161017f91d907dba07ebcdff0
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2025-04-21 18:16:23 +0300

    Update isMemberOfGroup.js

--- utils/isMemberOfGroup.js
+++ utils/isMemberOfGroup.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 // const BOT_TOKEN = '7639349507:AAEMg5R2hjjFZv1ByT_1aDZeNR9kY_Wc460';
-
+const groupChatId = -1002323508017;
 
 module.exports = async function isMemberOfGroup(userId) {
     try {
