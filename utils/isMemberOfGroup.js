638c1793a9d4c54792dd392ae6d73df2457fc112
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2025-04-21 18:19:00 +0300

    Update isMemberOfGroup.js

--- utils/isMemberOfGroup.js
+++ utils/isMemberOfGroup.js
@@ -4,7 +4,7 @@ const groupChatId = -1002323508017;
 
 module.exports = async function isMemberOfGroup(userId) {
     try {
-        const res = await axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/getChatMember`, {
+        const res = await axios.get(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/getChatMember`, {
             params: { chat_id: groupChatId , user_id: userId }
         });
         const memberStatus = res.data?.result?.status;
